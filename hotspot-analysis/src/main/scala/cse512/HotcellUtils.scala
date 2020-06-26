a014c0e4769deb130371d72b16d09278c8b0216d
willcray <will.c.cray@gmail.com>
2020-06-26 16:44:02 -0500

    Added getNeighbors() and checkInBounds() function to support mapping operation. Unsure if they work.

--- hotspot-analysis/src/main/scala/cse512/HotcellUtils.scala
+++ hotspot-analysis/src/main/scala/cse512/HotcellUtils.scala
@@ -3,6 +3,7 @@ package cse512
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import org.apache.commons.collections.functors.TruePredicate
 
 object HotcellUtils {
   val coordinateStep = 0.01
@@ -47,5 +48,67 @@ object HotcellUtils {
     return calendar.get(Calendar.DAY_OF_MONTH)
   }
 
-  // YOU NEED TO CHANGE THIS PART
+  def checkInBounds (bounds: List[Float], pointX: Int, pointY: Int, pointZ: Int): Boolean =
+  {
+    var minX = bounds(0)
+    var maxX = bounds(1)
+    var minY = bounds(2)
+    var maxY = bounds(3)
+    var minZ = bounds(4)
+    var maxZ = bounds(5)
+
+    if (pointX >= minX && pointX <= maxX && pointY >= minY && pointY <= maxY && pointZ >= minZ && pointZ <= maxZ)
+    {
+      return true
+    }
+    else
+    {
+      return false
+    }
+  }
+
+  // takes point in table and returns list of nearest neighbors, accounting bounds of rectangle
+  def getNeighbors (bounds: List[Float], pointX: Int, pointY: Int, pointZ: Int, count: Int): List[(Int, Int, Int)] =
+  {
+    var neighbors = List.newBuilder[(Int, Int, Int)]
+    // iterate each possible point combination
+    // each combination has the following options for each dimension: decrement, same, increment
+    // iterate point list and apply modifications to each dimension
+    var x = pointX
+    var y = pointY
+    var z = pointZ
+    for (i <- 0 to 2)
+    {
+      i match
+      {
+        case 0 => x -= 1
+        case 1 => x += 1
+        case 2 => x += 1
+      }
+      for (j <- 0 to 2)
+      {
+        j match
+        {
+          case 0 => y -= 1
+          case 1 => y += 1
+          case 2 => y += 1
+        }
+        for (k <- 0 to 2)
+        {
+          k match
+          {
+            case 0 => z -= 1
+            case 1 => z += 1
+            case 2 => z += 1
+          }
+          // add point to neighbors list if inbounds
+          if (checkInBounds(bounds, x, y, z))
+          {
+            neighbors += ((x, y, z))
+          }
+        }
+      }
+    }
+    return neighbors.result()
+  }
 }
